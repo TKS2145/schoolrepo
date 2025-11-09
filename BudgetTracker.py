eef203ba6c85e8bf4413d0d31c58d49223f22cfb
TKS2145 <t.sauntoo@alustudent.com>
2025-11-09 11:13:42 +0400

    Added main function

---
+++ BudgetTracker.py
@@ -0,0 +1,18 @@
+#TKS Budget Tracker
+print("Welcome to the budget tracker of TKS")
+print("/n")
+
+def main():
+    option = OptionScreen()
+
+def OptionScreen():
+
+    print("Type the number of the option which you want to choose")
+    print("1) Add income")
+    print("2) Add expense")
+    print("3) Filter")
+    print("4) Show summary")
+    print("0) exit")
+    option = input("Choose option")
+    return option
+
