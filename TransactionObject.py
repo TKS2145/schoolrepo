d18165934f5f830f44428bba360f12832af1b62f
TKS2145 <t.sauntoo@alustudent.com>
2025-11-23 20:14:49 +0400

    Created transaction object

---
+++ TransactionObject.py
@@ -0,0 +1,7 @@
+class transaction(NewDate, NewAmount, NewCategory, NewDesciptom):
+    def __init__(self):
+        Date = NewDate
+        Amount = NewAmount
+        Category = NewCategory
+        Desciption = NewDescription
+
