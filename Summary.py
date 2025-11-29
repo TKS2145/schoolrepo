ced9e67ad6254cdaab2df479597524f9e9119f63
TKS2145 <t.sauntoo@alustudent.com>
2025-11-29 19:17:31 +0400

    Finished summary module

--- Summary.py
+++ Summary.py
@@ -1,2 +1,41 @@
+from Global import IncomeList
+from Global import ExpenseList
+
 def Summary():
-    print("Wazaaa")
+    
+
+    if  len(IncomeList) == 0:
+        print("There are no Incomes.")
+
+    if len(ExpenseList) == 0:
+       print("There are no expenses.")
+
+    if len(IncomeList) == 0 and len(ExpenseList) == 0: #If there are no transactions, no need to continue
+        return
+    
+    else :
+
+        print("There are " , len(IncomeList) , " incomes and " , len(ExpenseList) , " expenses.") #If transactions were entered, number and types of transactions are outputed.
+
+        totalincome = 0.0
+        totalexpense = 0.0
+
+        for i in IncomeList:
+            totalincome += IncomeList[i].amount
+
+        for i in ExpenseList:
+            totalexpense += ExpenseList[i].amount
+
+        print("Total incomes: " , totalincome)
+        print("Total expenses: " , totalexpense)
+
+        difference = totalincome - totalexpense
+
+        if difference > 0: #If difference > 0, income > expenses so money saved
+            print("Amount saved: " , difference)
+        elif difference < 0:    #if difference < 0, expenses > incomes so money is due
+            print("Amount due: " , -difference)
+        else:
+            print("All money obtained has been spent") #Rare case income == expense
+
+        return #Ending the program
